64aaf6389ee36b5005a6381f280c2a3f8c93fc0c
Jhonis Santos dos Anjos <jhonis_10@hotmail.com.br>
2021-08-18 21:09:43 -0300

    Project created

---
+++ src/main/java/dev/jhonis/ecommerce/payment/EcPaymentApplication.java
@@ -0,0 +1,13 @@
+package dev.jhonis.ecommerce.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jhonis/ecommerce/payment/EcPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jhonis.ecommerce.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
